d6afc36f163eb720995908f774578326216cf5c7
Bruno Alves <bruno-alves2010@live.com>
2023-03-05 15:58:39 -0300

    feat: add SignupController email validation
    * Ensure error message is returned if email is invalid

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,9 +1,13 @@
+import { InvalidParamError } from '../errors/invalid-param-error';
 import { MissingParamError } from '../errors/missing-param-error';
 import { badRequest } from '../helpers/http-helper';
 import type { Controller } from '../protocols/controller';
+import type { EmailValidator } from '../protocols/email-validator';
 import type { HttpRequest, HttpResponse } from '../protocols/http';
 
 export class SignupController implements Controller {
+  constructor (private readonly emailValidator: EmailValidator) { }
+
   handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation'];
 
@@ -11,6 +15,11 @@ export class SignupController implements Controller {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field));
       }
+    }
+
+    const isValid = this.emailValidator.isValid(httpRequest.body.email);
+    if (!isValid) {
+      return badRequest(new InvalidParamError('email'));
     }
   }
 }
