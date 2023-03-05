90452cc1aa17cf0571c8efe852cfa8cc52c5940e
Bruno Alves <bruno-alves2010@live.com>
2023-03-05 13:57:46 -0300

    refactor: add test.each

--- src/presentation/controllers/signup-controller.spec.ts
+++ src/presentation/controllers/signup-controller.spec.ts
@@ -2,63 +2,45 @@ import { MissingParamError } from '../errors/missing-param-error';
 import { SignupController } from './signup';
 
 describe('Signup Controller', () => {
-  test('Should return 400 if no name is provided', () => {
-    const sut = new SignupController();
-    const httpRequest = {
+  test.each([
+    {
       body: {
         email: 'any_email@mail.com',
         password: 'any_password',
         passwordConfirmation: 'any_password'
-      }
-    };
-    const httpResponse = sut.handle(httpRequest);
-
-    expect(httpResponse.statusCode).toBe(400);
-    expect(httpResponse.body).toEqual(new MissingParamError('name'));
-  });
-
-  test('Should return 400 if no email is provided', () => {
-    const sut = new SignupController();
-    const httpRequest = {
+      },
+      field: 'name'
+    },
+    {
       body: {
         name: 'any_name',
         password: 'any_password',
         passwordConfirmation: 'any_password'
-      }
-    };
-    const httpResponse = sut.handle(httpRequest);
-
-    expect(httpResponse.statusCode).toBe(400);
-    expect(httpResponse.body).toEqual(new MissingParamError('email'));
-  });
-
-  test('Should return 400 if no password is provided', () => {
-    const sut = new SignupController();
-    const httpRequest = {
+      },
+      field: 'email'
+    },
+    {
       body: {
         name: 'any_name',
-        email: 'any_email@email.com',
+        email: 'any_email@emal.com',
         passwordConfirmation: 'any_password'
-      }
-    };
-    const httpResponse = sut.handle(httpRequest);
-
-    expect(httpResponse.statusCode).toBe(400);
-    expect(httpResponse.body).toEqual(new MissingParamError('password'));
-  });
-
-  test('Should return 400 if no passwordConfirmation is provided', () => {
-    const sut = new SignupController();
-    const httpRequest = {
+      },
+      field: 'password'
+    },
+    {
       body: {
         name: 'any_name',
-        email: 'any_email@email.com',
+        email: 'any_email@emal.com',
         password: 'any_password'
-      }
-    };
+      },
+      field: 'passwordConfirmation'
+    }
+  ])('Should return 400 if no $field is provided', ({ body, field }) => {
+    const sut = new SignupController();
+    const httpRequest = { body };
     const httpResponse = sut.handle(httpRequest);
 
     expect(httpResponse.statusCode).toBe(400);
-    expect(httpResponse.body).toEqual(new MissingParamError('passwordConfirmation'));
+    expect(httpResponse.body).toEqual(new MissingParamError(field));
   });
 });
