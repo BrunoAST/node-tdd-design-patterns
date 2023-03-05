8e45b55d9d1380878582e69ac760dce1b33e2a35
Bruno Alves <bruno-alves2010@live.com>
2023-03-05 13:47:32 -0300

    refactor: implement controller inteface

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,9 @@
 import { MissingParamError } from '../errors/missing-param-error';
 import { badRequest } from '../helpers/http-helper';
+import type { Controller } from '../protocols/controller';
 import type { HttpRequest, HttpResponse } from '../protocols/http';
 
-export class SignupController {
+export class SignupController implements Controller {
   handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation'];
 
