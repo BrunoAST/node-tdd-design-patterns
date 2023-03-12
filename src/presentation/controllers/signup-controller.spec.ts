6cb4290e9ac9768bf85b77848a3c57e25b0c4da0
Bruno Alves <bruno-alves2010@live.com>
2023-03-12 11:09:51 -0300

    refactor: add spy for error validatidor

--- src/presentation/controllers/signup-controller.spec.ts
+++ src/presentation/controllers/signup-controller.spec.ts
@@ -18,16 +18,6 @@ const makeEmailValidator = (): EmailValidator => {
   return new EmailValidatorStub();
 };
 
-const makeEmailValidatorWithError = (): EmailValidator => {
-  class EmailValidatorStub implements EmailValidator {
-    isValid (email: string): boolean {
-      throw new Error();
-    }
-  }
-
-  return new EmailValidatorStub();
-};
-
 const makeSut = (): SutTypes => {
   const emailValidatorStub = makeEmailValidator();
 
@@ -123,8 +113,10 @@ describe('Signup Controller', () => {
     });
 
     test('Should return 500 if EmailValidator throws', () => {
-      const emailValidatorStub = makeEmailValidatorWithError();
-      const sut = new SignupController(emailValidatorStub);
+      const { sut, emailValidatorStub } = makeSut();
+      jest.spyOn(emailValidatorStub, 'isValid').mockImplementation(() => {
+        throw new Error();
+      });
 
       const httpRequest = {
         body: {
