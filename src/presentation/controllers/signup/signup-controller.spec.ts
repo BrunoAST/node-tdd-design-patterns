de82c0a82d555216aae559a09f1c347a4354e30e
Bruno Alves <bruno-alves2010@live.com>
2023-03-12 11:51:36 -0300

    test: add SignupController validation
    * Ensure it returns 500 if AddAccount throws

--- src/presentation/controllers/signup/signup-controller.spec.ts
+++ src/presentation/controllers/signup/signup-controller.spec.ts
@@ -194,5 +194,25 @@ describe('Signup Controller', () => {
         password: 'any_password'
       });
     });
+
+    test('Should return 500 if AddAccount throws', () => {
+      const { sut, addAccountStub } = makeSut();
+      jest.spyOn(addAccountStub, 'add').mockImplementation(() => {
+        throw new Error();
+      });
+
+      const httpRequest = {
+        body: {
+          name: 'any_name',
+          email: 'any_email@mail.com',
+          password: 'any_password',
+          passwordConfirmation: 'any_password'
+        }
+      };
+      const httpResponse = sut.handle(httpRequest);
+
+      expect(httpResponse.statusCode).toBe(500);
+      expect(httpResponse.body).toEqual(new ServerError());
+    });
   });
 });
