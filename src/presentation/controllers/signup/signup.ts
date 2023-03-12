6c464fa418b9ffde3ec430222bf8916abb96592e
Bruno Alves <bruno-alves2010@live.com>
2023-03-12 12:07:17 -0300

    refactor: add ok helper to SignUp controller

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -1,5 +1,5 @@
 import { InvalidParamError, MissingParamError } from '../../errors';
-import { badRequest, serverError } from '../../helpers/http-helper';
+import { badRequest, ok, serverError } from '../../helpers/http-helper';
 import type {
   AddAccount,
   Controller,
@@ -36,10 +36,7 @@ export class SignupController implements Controller {
       }
 
       const account = this.addAccount.add({ name, email, password });
-      return {
-        statusCode: 200,
-        body: account
-      };
+      return ok(account);
     } catch (error) {
       return serverError();
     }
